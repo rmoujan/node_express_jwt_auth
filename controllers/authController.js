d01f92e4f118568ef55f944c56ae32173294dba2
rmoujan <rmoujan@student.1337.ma>
2024-04-23 12:45:29 +0100

    adding a web token to the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
 // handle errors :
 const handleErrors = (err) => {
     console.log(err.message, err.code);
@@ -18,6 +18,14 @@ const handleErrors = (err) => {
     }
     return errors;
 }
+const maxAge = 3 * 24 * 60 * 60; // 3 days in secondes .
+// this jwttoken will be valid for 3 days.
+// this function create a token by id will the body and header will be added automatically.
+const createToken = (id) => {
+    return jwt.sign({ id }, 'reshe secret', {
+        expiresIn:maxAge,
+    });
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -29,9 +37,14 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
+    console.log('From the server side ************ ',email, password);
     try {
         const newUser = await User.create({ email, password });
-        res.status(201).json(newUser);  
+        const token = createToken(newUser._id);
+        // adding the jwtToken in the cookie:
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000}); 
+        // {httpOnly: true} ==> this option disable to access the cookie in the frontend. 
+        res.status(201).json({user:newUser._id});  
     }
     catch(err)
     {
@@ -42,7 +55,6 @@ module.exports.signup_post = async (req, res) => {
          
     }
 
-    console.log(email, password);
     // res.send('new signup');
 }
 
